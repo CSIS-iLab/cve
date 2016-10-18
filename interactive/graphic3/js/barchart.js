5766fb8d365e94d76f94569ff7e1e3c49102a01d
willahine <whine@Willas-Mac-Pro.local>
2016-10-18 15:58:44 -0400

    graphic 3

---
+++ interactive/graphic3/js/barchart.js
@@ -0,0 +1,112 @@
+$(function() {
+  Highcharts.setOptions({
+      colors: ['#65c8cd', '#e51d26 ']
+  });
+  $('#hcContainer').highcharts({
+    data: {
+      googleSpreadsheetKey: '1UtV_MiBdbISiyxS5gZSiNoIPsiQ6rTGHcF57C69_N5g',
+    },
+
+      chart: {
+        type: 'column',
+        backgroundColor: '#272b31',
+        border: 'none',
+        color: '#000',
+        plotShadow: true
+      },
+      credits: {
+        enabled: true,
+        href: "#",
+        text: 'CSIS CVE | Source: Mark Penn',
+        style: {
+          cursor: 'pointer',
+          color: '#ffffff',
+          fontSize: '10px'
+        }
+      },
+
+      title: {
+        text: "Violent Extremism Seen as a Major Problem in Most Countries",
+        style: {
+          color: '#ffffff',
+          fontWeight: '500',
+          fontSize:'24px',
+          letterSpacing: '0.5px'
+        }
+      },
+      subtitle: {
+        text: 'Overall, around two-thirds see violent extremism as being a major problem in their country.',
+        style: {
+          color: '#ffffff',
+        }
+      },
+      xAxis: {
+        labels: {
+          style: {
+            color: '#ffffff',
+            fontSize: '14px'
+          }
+        },
+        categories: [ "Global", "Turkey", "Indonesia", "France", "India", "Egypt", "US","UK", "China" ],
+        crosshair: true
+      },
+      yAxis: {
+         gridLineWidth: 0,
+        min: 0,
+        labels: {
+          enabled: true,
+          style: {
+            color: '#ffffff',
+            fontSize: '14px'
+          }
+        },
+        title: {
+          text: null,
+          style: {
+            color: '#ffffff',
+            fontWeight: 'bold',
+            fontSize: '16px'
+          }
+        }
+
+      },
+      tooltip: {
+        backgroundColor: '#FFF',
+        style: {
+          color: '#000',
+          fontWeight: 'bol'
+        },
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+        '<td style="padding:0"><b>{point.y} %</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
+      legend: {
+        title: {
+          text: 'Source<br/><span style="font-size: 9px; color: #666; font-weight: normal">(Click to hide)</span>',
+          style: {
+            fontStyle: 'italic',
+            color: '#ffffff'
+          }
+        },
+        align: 'left',
+        verticalAlign: 'bottom',
+        layout: 'horizontal',
+        itemStyle: {
+          color: '#ffffff'
+        },
+        itemHoverStyle: {
+          color: '#ffffff'
+        }
+      },
+
+    });
+  });
